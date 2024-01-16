9970d95f24d5397cb211f3982c260c140bc29e73
jonah-kayiwa <152336281+jonah-kayiwa@users.noreply.github.com>
2024-01-16 15:51:40 +0300

    completed

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverseString = word.split('').reverse().join('');
+
+  if (word === reverseString) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  initialize a function isPalindrome with an argument word
+
+  convert the word into an array, reverse the array values, and convert the reverse array values into a string
+
+  run a condition to see if the string and the reverse string are equal and if so, return true else return false
 */
 
 /*
   Add written explanation of your solution here
+  The function isPalindrome takes an argument word, our input in this case is a string. 
+  The string is then converted into an array and the values reversed then converted again into a reversestring
+  The reversestring is then compared to the original string and if true then we return true else return false
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,10 +31,57 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("civic"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("viki"));
+}
+
+module.exports = isPalindrome;function isPalindrome(word) {
+  // Write your algorithm here
+  const reverseString = word.split('').reverse().join('');
+
+  if (word === reverseString) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/* 
+  Add your pseudocode here
+  initialize a function isPalindrome with an argument word
+
+  convert the word into an array, reverse the array values, and convert the reverse array values into a string
+
+  run a condition to see if the string and the reverse string are equal and if so, return true else return false
+*/
+
+/*
+  Add written explanation of your solution here
+  The function isPalindrome takes an argument word, our input in this case is a string. 
+  The string is then converted into an array and the values reversed then converted again into a reversestring
+  The reversestring is then compared to the original string and if true then we return true else return false
+*/
+
+// You can run `node index.js` to view these console logs
+if (require.main === module) {
+  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("civic"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("viki"));
 }
 
 module.exports = isPalindrome;
